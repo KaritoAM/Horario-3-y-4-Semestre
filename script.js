6ca7b003bcee6ef8ecf177c83705a36137f3addc
KaritoAM <caroline_acevedo@live.com>
2026-07-24 18:27:31 -0400

    Change course start time from 14:30 to 12:30

--- script.js
+++ script.js
@@ -441,7 +441,7 @@ const horariosData = [
         "nrc": 9417,
         "nombre_curso": "Análisis Instrumental",
         "dia": "LUNES",
-        "hora_inicio": "14:30:00",
+        "hora_inicio": "12:30:00",
         "hora_fin": "15:50:00",
         "profesor": "Alejandra Molina",
         "tipo": "Laboratorio"
